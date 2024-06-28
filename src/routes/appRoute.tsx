8c09db6a21bbccff299237ad0ad81700a17b49f7
berkan bala <berkan.balaa@gmail.com>
2024-06-28 17:24:13 +0300

    scss files changed

--- src/routes/appRoute.tsx
+++ src/routes/appRoute.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from "../pages/home/home";
 import { PokemonDetails } from "../pages/pokemonDetails/pokemonDetails";
 import { AppLayout } from "../layout/appLayout/appLayout";
+import { NotFound } from "../pages/notFound/notFound";
+
 export const AppRoute = () => {
   return (
     <Router>
@@ -9,6 +11,7 @@ export const AppRoute = () => {
         <Route element={<AppLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/details/:id" element={<PokemonDetails />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </Router>
